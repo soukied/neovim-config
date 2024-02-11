9d02e7b5a257441b11c3f6ab1fcc81b82aaae0cc
Adhya Adam Sulthan <adhyaadam@gmail.com>
2024-02-12 02:15:21 +0800

    Minor changes

--- lua/load_lsp.lua
+++ lua/load_lsp.lua
@@ -30,12 +30,12 @@ local lspkind = require('lspkind')
 cmp.setup({
 	formatting = {
 		expandable_indicator = true,
-		fields = { "kind", "menu", "abbr" },
+		fields = { "kind", "abbr", "menu"},
 		format = lspkind.cmp_format({
-			mode = 'symbol',  -- show only symbol annotations
-			maxwidth = 50,    -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-			ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-			show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+			mode = 'symbol',
+			maxwidth = 20,
+			ellipsis_char = '...',
+			show_labelDetails = true,
 			before = function(_, vim_item)
 				return vim_item
 			end
@@ -43,7 +43,7 @@ cmp.setup({
 	},
 	snippet = {
 		expand = function(args)
-			vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+			vim.fn["vsnip#anonymous"](args.body)
 		end,
 	},
 	window = {},
@@ -64,13 +64,19 @@ cmp.setup({
 		end, { "i", "s" }),
 		['<C-b>'] = cmp.mapping.scroll_docs(-4),
 		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-Space>'] = cmp.mapping(function(_)
+			if not cmp.visible() then
+				cmp.complete()
+			else
+				cmp.close()
+			end
+		end, { 'i', 's' }),
 		['<C-e>'] = cmp.mapping.abort(),
-		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		['<CR>'] = cmp.mapping.confirm({ select = true }),
 	}),
 	sources = cmp.config.sources({
 		{ name = 'nvim_lsp' },
-		{ name = 'vsnip' }, -- For vsnip users.
+		{ name = 'vsnip' },
 	}, {
 		{ name = 'buffer' },
 	})
@@ -79,7 +85,7 @@ cmp.setup({
 -- Set configuration for specific filetype.
 cmp.setup.filetype('gitcommit', {
 	sources = cmp.config.sources({
-		{ name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+		{ name = 'git' },
 	}, {
 		{ name = 'buffer' },
 	})
